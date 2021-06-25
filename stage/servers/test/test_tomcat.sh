d66d83204eee995e4254f1e571f537af3c9d8b23
clone8 <ihor.prots.lviv@gmail.com>
2021-06-25 11:55:36 -0400

    Fri 25 Jun 2021 11:55:36 AM EDT add war app simple-test

--- stage/servers/test/test_tomcat.sh
+++ stage/servers/test/test_tomcat.sh
@@ -1,8 +1,18 @@
 #! /bin/bash
-CODE=$(curl -o /dev/null -s --max-time 2 -w "%{http_code}\n" http://$1:8080)
-if [[ $CODE == "200" ]]; then
-	exit 0
-else
-	echo "Request CODE:\t$CODE"
-	exit 1
-fi
+
+function testing(){
+
+	CODE=$(curl -o /dev/null -s --max-time 2 -w "%{http_code}\n" $1)
+	if [[ $CODE == "200" ]]; then
+		echo "URL $1 is OK!"
+	else
+		echo "Request CODE: $CODE"
+		echo "Bad URL: $1"
+		exit 1
+	fi
+}
+
+
+
+testing "http://$1:8080"
+testing "http://$1:8080/SampleWebApp/"
